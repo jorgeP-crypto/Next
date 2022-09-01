55d4bd1902f4acbed38f439f6de08c1cf982124f
George Antwi-Agyei <gantwiagyei58@gmail.com>
2022-09-01 09:03:11 +0000

    No significant changes made just the GUI which has been disabled

--- gui.py
+++ gui.py
@@ -1,21 +1,21 @@
-from tkinter import *
-from main import *
+# from tkinter import *
+# from main import *
 
-def main_screen():
-    global screen
-    screen = Tk()
-    screen.title(name_assistant)
-    screen.geometry("700x550")
-    screen.iconbitmap('.\app_icon.ico')
+# def main_screen():
+#     global screen
+#     screen = Tk()
+#     screen.title(name_assistant)
+#     screen.geometry("700x550")
+#     screen.iconbitmap('.\app_icon.ico')
 
-    name_label = Label(text=name_assistant, width=300, bg="black", fg="white", font=("Calibri", 13))
-    name_label.pack()
+#     name_label = Label(text=name_assistant, width=300, bg="black", fg="white", font=("Calibri", 13))
+#     name_label.pack()
 
-    microphone_photo = PhotoImage(file="assistant_logo.png")
-    microphone_button = Button(image=microphone_photo, command=Process_audio)
-    microphone_button.pack(pady=10)
+#     microphone_photo = PhotoImage(file="assistant_logo.png")
+#     microphone_button = Button(image=microphone_photo, command=Process_audio)
+#     microphone_button.pack(pady=10)
 
-    info_button = Button(text="Info", command=info)
-    info_button.pack(pady=10)
+#     info_button = Button(text="Info", command=info)
+#     info_button.pack(pady=10)
 
-    screen.mainloop()
+#     screen.mainloop()
--- main.py
+++ main.py
@@ -582,42 +582,42 @@ def Process_audio():
 
 Process_audio()
 
-def info():
-    info_screen = Toplevel(screen)
-    info_screen.title("Info")
-    info_screen.iconbitmap('app_icon.ico')
+# def info():
+#     info_screen = Toplevel(screen)
+#     info_screen.title("Info")
+#     info_screen.iconbitmap('app_icon.ico')
 
-    creator_label = Label(info_screen, text="Created by George Antwi")
-    creator_label.pack()
+#     creator_label = Label(info_screen, text="Created by George Antwi")
+#     creator_label.pack()
 
-    age_label = Label(info_screen, text=" at the age of 12")
-    age_label.pack()
+#     age_label = Label(info_screen, text=" at the age of 12")
+#     age_label.pack()
 
-    for_label = Label(info_screen, text="For Makers-pace")
-    for_label.pack()
-
-
-def main_screen():
-    global screen
-    # name_assistant = "Next"
-    screen = Tk()
-    screen.title(name_assistant)
-    screen.geometry("700x550")
-    screen.iconbitmap('.\app_icon.ico')
-
-    name_label = Label(text=name_assistant, width=300, bg="black", fg="white", font=("Calibri", 13))
-    name_label.pack()
-
-    microphone_photo = PhotoImage(file="assistant_logo.png")
-    microphone_button = Button(image=microphone_photo, command=Process_audio)
-    microphone_button.pack(pady=10)
-
-    info_button = Button(text="Info", command=info)
-    info_button.pack(pady=10)
-
-    screen.mainloop()
+#     for_label = Label(info_screen, text="For Makers-pace")
+#     for_label.pack()
 
 
-main_screen()
+# def main_screen():
+#     global screen
+#     # name_assistant = "Next"
+#     screen = Tk()
+#     screen.title(name_assistant)
+#     screen.geometry("700x550")
+#     screen.iconbitmap('.\app_icon.ico')
+
+#     name_label = Label(text=name_assistant, width=300, bg="black", fg="white", font=("Calibri", 13))
+#     name_label.pack()
+
+#     microphone_photo = PhotoImage(file="assistant_logo.png")
+#     microphone_button = Button(image=microphone_photo, command=Process_audio)
+#     microphone_button.pack(pady=10)
+
+#     info_button = Button(text="Info", command=info)
+#     info_button.pack(pady=10)
+
+#     screen.mainloop()
+
+
+# main_screen()
 
 
